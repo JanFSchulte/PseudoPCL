580f013a159025bd7e009d7051b2388aada759f3
Knut Kiesel <kiesel@cern.ch>
2016-05-18 13:52:39 +0200

    make directory first

--- cleanHouse.sh
+++ cleanHouse.sh
@@ -4,6 +4,7 @@ rm milleBinary_*
 cd ../
 rm -r MinBias_2016_*
 cd ../
+mkdir -p ~/public/pp3.8T_PCL_Alignment
 mv Results$RUNNUM ~/public/pp3.8T_PCL_Alignment
 cp printTwiki.py ~/public/pp3.8T_PCL_Alignment
 echo "DONE!"
